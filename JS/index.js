cd10a4299828bbac1fe41b67bcc2effd8ea995f7
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-18 12:07:24 +0100

    the right letters are coming up

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,10 @@ var answers = ["fastandfurious","namibia","shondarhimes"]
 // random gen of answers array
 var randomgen = answers[Math.floor(Math.random()* answers.length)];
 console.log(randomgen);
+
+// turning the lower case to upper so one can compare later
+randomgen = randomgen.toUpperCase()
+
 // turn the string of random word into an array of its elements (so we can compare)
 var splitArray = randomgen.split("");
 // Putting clicks variable up here so it can be accessed in function and in DOM
@@ -14,11 +18,11 @@ var maxtries = 8;
  for (var i = 0; i < randomgen.length; i++) {
  answerArray[i] ="_"
   };
+  // Place to store the letter
+var storeLetter = ""
 console.log(answerArray);
 console.log(splitArray);
 
-// store letter
-var storeLetter = ""
 // display the underscores in the right place
 document.getElementById("underscores").innerHTML = answerArray ;
 console.log(randomgen.length);
@@ -27,41 +31,41 @@ console.log(randomgen.length);
 document.addEventListener('DOMContentLoaded',function(){
    clicks = document.getElementsByTagName("button");
     for (var i = 0; i < clicks.length; i++) {
-      // pushing the values of clicks var into valuesArray
       clicks[i].addEventListener("click", function(event){
+        // variable store Letter store this.value temporarily
         storeLetter = this.value;
         replaceLetters();
-        // console.log(clicks);
-        // console.log(this.value);
-
+        // after function called for whatever to store on the screen
+        // replace underscores with the right letter
+        // document.getElementById("underscores").innerHTML += storeLetter;
       });
     };
 });
 
-
 // function to replace underscore with correct letters if inocrrevct loose a life
 function replaceLetters(event){
   var goodattempt = false;
-  // console.log(storeLetter + " inside replaceLetter");
-  console.log(storeLetter);
-  // console.log(splitArray.length);
   for (var j = 0; j<splitArray.length; j++){
     // console.log(valuesArray[j]);
     // console.log(splitArray[j]);
+    // console.log("ST"+storeLetter);
     if (storeLetter == splitArray[j]){
-      answerArray[j] == splitArray[j]
+      answerArray[j] = splitArray[j]
+      console.log("inside"+storeLetter);
+      console.log(answerArray);
       goodattempt = true;
       // when I click a button alphabet until == splitArray[j] shows
       // and disable the button
     }
-    // when I click any letter the value of A and M  comes up and I don't know why
-    // console.log(valuesArray);
-    // console.log(valuesArray[j]);
-  return goodattempt;
-}
-if (replaceLetters()==false) {
-  maxtries = maxtries-1
-};
-};
 
-// replaceLetters();
+  }
+    console.log(answerArray);
+    document.getElementById("underscores").innerHTML = answerArray;
+    return goodattempt;
+  if (replaceLetters()==false) {
+    maxtries = maxtries-1
+    alert("You have ran out of lives without guessing correctly. The word was + randomgen")
+    document.getElementById('lives').innerHTML = maxtries;
+  };
+  };
+  console.log(splitArray);
